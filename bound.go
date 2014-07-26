5a3fd0ff7d458104ee462b6e01f62c910f3d82ef
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2014-07-26 14:35:10 +0200

    package instead of cli

--- bound.go
+++ bound.go
@@ -1,4 +1,4 @@
-package main
+package gofish
 
 import (
 	"fmt"
--- pond_test.go
+++ pond_test.go
@@ -1,4 +1,4 @@
-package main
+package gofish
 
 import (
 	// "github.com/paddie/fish"
@@ -7,42 +7,29 @@ import (
 	"math"
 	"math/rand"
 	"runtime"
-	"testing"
+	// "testing"
+	"flag"
 )
 
 var pond *Pond
 
+var (
+	imm_size int
+	mut_size int
+	freq     int
+	steps    int
+	workers  int
+	cpu      int
+)
+
 func init() {
+	// flag.IntVar(&imm_size, "i", 0, "define the size in bytes of the immutable part")
+	// flag.IntVar(&mut_size, "m", 0, "define the size in bytes of the immutable part")
+	// flag.IntVar(&freq, "f", 50, "define sync frequency")
+	flag.IntVar(&steps, "s", 1000, "number of simulation steps")
+	flag.IntVar(&workers, "w", 10, "number of workers")
+	flag.IntVar(&cpu, "cpu", 1, "GOMAXPROCS")
 
-	runtime.GOMAXPROCS(2)
-
-	count := 1000
-	pct := 10.0
-	factor := int(float64(count) / pct)
-	speed := 0.3
-
-	fmt.Println("informed factor: ", factor)
-
-	c := Point{500, 160}
-
-	ps := RandomPoints(c, 200, count)
-
-	fish := make([]*Fish, 0, count)
-
-	for i, p := range ps {
-		dir := Vector2D{0.1, 0.0}
-		info := Vector2D{0.0, 5.0}
-		tmp := NewFish(i+1, p, dir, speed)
-
-		if (i+1)%factor == 0 {
-			tmp.Inform(info)
-		}
-		fish = append(fish, tmp)
-
-	}
-
-	bound, _ := NewBound(Point{0, 0}, Point{1000, 1000})
-	pond, _ = NewPond(bound, fish)
 }
 
 func RandomPoints(c Point, maxRadius float64, count int) []Point {
@@ -70,8 +57,41 @@ func RandomPoints(c Point, maxRadius float64, count int) []Point {
 	return points
 }
 
-func TestConcurrentSimulation(t *testing.T) {
-	steps := 200
+func main() {
+	flag.Parse()
 
-	pond.Simulate(steps, 100)
+	runtime.GOMAXPROCS(cpu)
+
+	fmt.Println("steps = ", steps)
+	fmt.Println("workers = ", workers)
+	// fmt.Println("SyncFreqency = ", freq)
+
+	count := 1000
+	pct := 10.0
+	factor := int(float64(count) / pct)
+	speed := 0.3
+
+	fmt.Println("informed factor: ", factor)
+
+	c := Point{500, 160}
+
+	ps := RandomPoints(c, 200, count)
+
+	fish := make([]*Fish, 0, count)
+
+	for i, p := range ps {
+		dir := Vector2D{0.1, 0.0}
+		info := Vector2D{0.0, 5.0}
+		tmp := NewFish(i+1, p, dir, speed)
+
+		if (i+1)%factor == 0 {
+			tmp.Inform(info)
+		}
+		fish = append(fish, tmp)
+	}
+
+	bound, _ := NewBound(Point{0, 0}, Point{1000, 1000})
+	pond, _ = NewPond(bound, fish)
+
+	pond.Simulate(steps, workers)
 }
